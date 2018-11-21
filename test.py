53cc3c702c8a2f569ac03883e7ef63e8ccfc2aad
ACG333 <44452066+ACG333@users.noreply.github.com>
2018-11-21 13:30:37 -0500

    Update test.py

--- test.py
+++ test.py
@@ -5,3 +5,11 @@ import matplotlib
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 print("hola mundo cruel")
+
+dat=np.loadtxt('data.txt', delimiter=',')
+x=dat[:,0]
+y=dat[:,1]
+
+
+plt.plot(x,y)
+plt.savefig('plot.png')
